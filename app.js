edccc7b79578bf880af5c3e9b44edd6fa214eaa4
Johanna Schreiner <jschreinerdesign@gmail.com>
2017-12-15 09:48:50 +1100

    refactored

--- app.js
+++ app.js
@@ -1,70 +1,99 @@
-var s0 = document.querySelector("#s0")
-var s1 = document.querySelector("#s1")
-var s2 = document.querySelector("#s2")
-var s3 = document.querySelector("#s3")
-var s4 = document.querySelector("#s4")
-var s5 = document.querySelector("#s5")
-var s6 = document.querySelector("#s6")
-var s7 = document.querySelector("#s7")
-var s8 = document.querySelector("#s8")
 
-var squares = [s0, s1, s2, s3, s4, s5, s6, s7, s8]
+var squares = document.querySelectorAll(".tile")
 
 var counter = 0
 var player = ""
 
+var originalColor = '#EAF2F8'
+var p1Color = '#2471A3'
+var p2Color = '#7FB3D5'
+var p1 = 'P1'
+var p2 = 'P2'
+var gameFinished = false
+
 
 function fillSquare(event){
   if(counter % 2 !== 0){
-    event.target.style.backgroundColor = '#2471A3'
-    player = "Player One"
+    event.target.style.backgroundColor = p1Color
+    player = p1
   } else{
-    event.target.style.backgroundColor = '#7FB3D5'
-    player = "Player Two"
+    event.target.style.backgroundColor = p2Color
+    player = p2
   } 
 } 
 
 var boardLog = new Array(9)
 
 function fillBoardLog(event){
-  boardLog[Number(event.target.id[1])] = event.target.style.backgroundColor
+  if(counter % 2 !== 0){
+    boardLog[Number(event.target.id[1])] = p1
+  } else{
+    boardLog[Number(event.target.id[1])] = p2
+  }
+  // console.log(boardLog)
+}
+
+var winner
+var winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+// function checkWinOrDraw(event){ 
+//   for(var i = 0; i < winConditions.length; i++){
+//     var winCombo = winConditions[i]
+//     for(var i = 0; i < winCombo.length; i++){
+//       if(winCombo[i] === player){
+//         endGame(player)
+//       }
+//     }
+//   }
+// }
+
+
+function checkWin(event){
+  for(var i = 0; i < winConditions.length; i++){
+    var winCombo = winConditions[i]
+    win = winCombo.every(function(index){
+      return boardLog[index] === player  
+    })
+    if(win){
+      endGame(player)
+      return true
+    }
+  }
+}
+
+// function checkWinOrDraw(event){
+//   var winCombo = winConditions[0]
+//   win = winCombo.every(function(index) {
+//     return boardLog[index] === player
+//   })
+//   if (win) {
+//     endGame(player)
+//   }
+// }
+
+function endGame(winner) {
+  console.log(winner + " wins!")
+  // alert(winner + " wins!")
+  gameFinished = true
+}
+
+function checkDraw(event){
+    if(!boardLog.includes(undefined)){
+      console.log("Players Draw!")
+      // alert("Players Draw!")
+    gameFinished = true
+    }
 }
 
 function makeMove(event){
   counter++
-  if(event.target.style.backgroundColor === ""){
+  if(event.target.style.backgroundColor === "" && !gameFinished){
     fillSquare(event)
     fillBoardLog(event)
-    checkWinDraw(event)
-
+    if(!checkWin(event)){
+      checkDraw(event)
   } 
 }
-
-var winner
-
-function checkWinDraw(event){ 
-  winner = player
-  if(boardLog.includes(undefined)){
-    if(boardLog[0] === boardLog[1] && boardLog[1] === boardLog[2] && boardLog[0] === boardLog[2] && boardLog[0] !== undefined){
-      alert(winner + " wins!")
-    } else if(boardLog[3] === boardLog[4] && boardLog[4] === boardLog[5] && boardLog[3] === boardLog[5] && boardLog[3] !== undefined){
-      alert(winner + " wins!")
-    } else if(boardLog[6] === boardLog[7] && boardLog[7] === boardLog[8] && boardLog[6] === boardLog[8] && boardLog[6] !== undefined){
-      alert(winner + " wins!")
-    } else if(boardLog[0] === boardLog[3] && boardLog[3] === boardLog[6] && boardLog[0] === boardLog[6] && boardLog[0] !== undefined){
-      alert(winner + " wins!")
-    } else if(boardLog[1] === boardLog[4] && boardLog[4] === boardLog[7] && boardLog[1] === boardLog[7] && boardLog[1] !== undefined){
-      alert(winner + " wins!")
-    } else if(boardLog[2] === boardLog[5] && boardLog[5] === boardLog[8] && boardLog[2] === boardLog[8] && boardLog[2] !== undefined){
-      alert(winner + " wins!")
-    } else if(boardLog[0] === boardLog[4] && boardLog[4] === boardLog[8] && boardLog[0] === boardLog[8] && boardLog[0] !== undefined){
-      alert(winner + " wins!")
-    } else if(boardLog[2] === boardLog[4] && boardLog[4] === boardLog[6] && boardLog[2] === boardLog[6] && boardLog[2] !== undefined){
-      alert(winner + " wins!")
-    } 
-  } else{
-    alert("Players Draw!")
-  }
 }
 
 for(var i = 0; i < squares.length; i++){
